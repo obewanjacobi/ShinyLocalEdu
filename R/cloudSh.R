9f80368211431ac4bcc3d99819e7eedf67734e6a
Homer White <hwhite0@georgetowncollege.edu>
2015-02-18 10:58:55 -0500

    experiment with animation speed options, permit passing of all standard arguments to cloud function

--- R/cloudSh.R
+++ R/cloudSh.R
@@ -3,12 +3,12 @@
 #' @description Uses the \code{cloud} function from the \code{lattice} package.
 #'
 #' @rdname cloudSh
-#' @usage cloudSh(x, data = parent.frame(),group=NULL)
+#' @usage cloudSh(x, data = parent.frame(),...)
 #' @param x Could be a formula.  If so, it should be of the form z~x*y|g1*g2, as in the
 #' \code{lattice} function \code{cloud}.
 #' @param data data frame supplying variables for formula x.  If variables in x are not found in the data,
 #' then they will be searched for in the parent environment.
-#' @param group A factor variable by which to groups the points of the plot.
+#' @param ... Other arguments passed to \code{cloud}.
 #' @return side effects
 #' @note This is a locally-run Shiny app.  It may not work properly on some R Studio Server set-ups,
 #' especially on the CentOS operating system.  For best views, open the app in the browser.
@@ -22,42 +22,96 @@
 #' cloudSh(Petal.Length ~ Sepal.Length * Sepal.Width,data=iris,group=Species)
 #' }
 cloudSh <-
-  function (x,data=parent.frame(),group=NULL)
+  function (x,data=parent.frame(),...)
   {
 
-  ll <- as.list(match.call(expand.dots = FALSE)[-1])
+  argList <- as.list(match.call(expand.dots = TRUE)[-1])
 
   haveGroup <- (length(as.character(substitute(group)))>0)
 
 # Define server logic for cloudSh app
-server <- shinyServer(function(input, output,session) {
+  server1 <- shinyServer(function(input, output,session) {
+    #This option uses slider to set step on controls.  Not properly responsive yet.
+
+    step <- reactive({
+      10*input$speed
+    })
+
+    output$zControl <- renderUI({
+      step <- step()
+      print(step)
+      sliderInput("zScreen","z",0,360,value=0,step=step,
+                  animate=animationOptions(interval=100,loop=TRUE))
+
+    })
+
+    output$xControl <- renderUI({
+      step <- step()
+      sliderInput("xScreen","x",0,360,value=90,step=step,
+                  animate=animationOptions(interval=100,loop=TRUE))
+
+    })
+
+    output$yControl <- renderUI({
+      step <- step()
+      sliderInput("yScreen","y",0,360,value=40,step=step,
+                  animate=animationOptions(interval=100,loop=TRUE))
+
+    })
+
+    output$cloudplot <- renderPlot({
+      if (!is.null(input$xScreen)) {
+        allArgs <- c(list(screen=list(x=-input$xScreen,y=input$yScreen,z=input$zScreen)),
+                     argList)
+        do.call(cloud,allArgs)
+        #       cloud(eval(ll$x),data=eval(ll$data),group=eval(ll$group),
+        #             screen=list(x=-input$xScreen,y=input$yScreen,z=input$zScreen),
+        #             auto.key=haveGroup,pch=19)
+      }
+    })
+
+
+  })
+server2 <- shinyServer(function(input, output,session) {
+  #This option permits three different step sizes.  Properly responsive.
+
+  step <- reactive({
+    switch(input$speed,
+      "slow"=1,
+      "medium"=10,
+      "fast"=30
+    )
+  })
 
   output$zControl <- renderUI({
-    speed <- 1000-9.99*input$speed
-    sliderInput("zScreen","z",0,360,value=0,step=1,
-                animate=animationOptions(interval=speed,loop=TRUE))
+    step <- step()
+    sliderInput("zScreen","z",0,360,value=0,step=step,
+                animate=animationOptions(interval=200,loop=TRUE))
 
   })
 
   output$xControl <- renderUI({
-    speed <- 1000-9.99*input$speed
-    sliderInput("xScreen","x",0,360,value=90,step=1,
-                animate=animationOptions(interval=speed,loop=TRUE))
+    step <- step()
+    sliderInput("xScreen","x",0,360,value=90,step=step,
+                animate=animationOptions(interval=200,loop=TRUE))
 
   })
 
   output$yControl <- renderUI({
-    speed <- 1000-9.99*input$speed
-    sliderInput("yScreen","y",0,360,value=40,step=1,
-                animate=animationOptions(interval=speed,loop=TRUE))
+    step <- step()
+    sliderInput("yScreen","y",0,360,value=40,step=step,
+                animate=animationOptions(interval=200,loop=TRUE))
 
   })
 
   output$cloudplot <- renderPlot({
     if (!is.null(input$xScreen)) {
-      cloud(eval(ll$x),data=eval(ll$data),group=eval(ll$group),
-            screen=list(x=-input$xScreen,y=input$yScreen,z=input$zScreen),
-            auto.key=haveGroup,pch=19)
+      allArgs <- c(list(screen=list(x=-input$xScreen,y=input$yScreen,z=input$zScreen)),
+                   argList)
+      do.call(cloud,allArgs)
+#       cloud(eval(ll$x),data=eval(ll$data),group=eval(ll$group),
+#             screen=list(x=-input$xScreen,y=input$yScreen,z=input$zScreen),
+#             auto.key=haveGroup,pch=19)
     }
   })
 
@@ -66,7 +120,7 @@ server <- shinyServer(function(input, output,session) {
 
 
 # Define ui for cloudSh app
-ui <- shinyUI(fluidPage(shinythemes::shinytheme("cerulean"),
+ui1 <- shinyUI(fluidPage(shinythemes::shinytheme("cerulean"),
 
   #  Application title
   titlePanel("3-D Scatter Plot"),
@@ -74,7 +128,7 @@ ui <- shinyUI(fluidPage(shinythemes::shinytheme("cerulean"),
   # Sidebar
   sidebarLayout(
     sidebarPanel(
-        sliderInput("speed","Animation Speed",1,100,step=.1,value=450),
+        sliderInput("speed","Animation Speed (degrees/sec)",0,360,step=10,value=90),
         uiOutput("zControl"),
         uiOutput("xControl"),
         uiOutput("yControl")
@@ -92,9 +146,34 @@ ui <- shinyUI(fluidPage(shinythemes::shinytheme("cerulean"),
 
 ))  #end fluidPage and and shinyUI
 
+ui2 <- shinyUI(fluidPage(shinythemes::shinytheme("cerulean"),
+
+                        #  Application title
+                        title="3-D Scatter Plot",
+                        titlePanel("3-D Scatter Plot"),
 
 
-shiny::shinyApp(ui = ui, server = server)
+                        plotOutput("cloudplot"),
+                        hr(),
+
+                        fluidRow(
+                          column(3,uiOutput("zControl")),
+                          column(3,offset=1,uiOutput("xControl")),
+                          column(3,offset=1,uiOutput("yControl"))
+                        ),
+
+                        fluidRow(
+                          column(3,offset=5,
+                                 selectInput("speed","Animation Speed",
+                                             c("Slow"="slow",
+                                                "Medium"="medium",
+                                               "Fast"="fast"))
+                          )
+                        )
+
+))  #end fluidPage and and shinyUI
+
+shiny::shinyApp(ui = ui2, server = server2)
 
 
   }#end cloudSh
