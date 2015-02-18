b3811ca58c0fa52c89592749c0231ee0ca9989a7
Homer White <hwhite0@georgetowncollege.edu>
2015-02-17 21:22:22 -0500

    add animation speed adjustment to cloudSh

--- R/cloudSh.R
+++ R/cloudSh.R
@@ -3,11 +3,12 @@
 #' @description Uses the \code{cloud} function from the \code{lattice} package.
 #'
 #' @rdname cloudSh
-#' @usage cloudSh(x, data = parent.frame())
+#' @usage cloudSh(x, data = parent.frame(),group=NULL)
 #' @param x Could be a formula.  If so, it should be of the form z~x*y|g1*g2, as in the
 #' \code{lattice} function \code{cloud}.
 #' @param data data frame supplying variables for formula x.  If variables in x are not found in the data,
 #' then they will be searched for in the parent environment.
+#' @param group A factor variable by which to groups the points of the plot.
 #' @return side effects
 #' @note This is a locally-run Shiny app.  It may not work properly on some R Studio Server set-ups,
 #' especially on the CentOS operating system.  For best views, open the app in the browser.
@@ -19,24 +20,44 @@
 #' \dontrun{
 #' }
 cloudSh <-
-  function (x,data=parent.frame())
+  function (x,data=parent.frame(),group=NULL)
   {
-
- ##################################################################
- ####
- ####
- #### Process Input
- ####
- ####
- #################################################################
-
-
+  groupName <- as.character(substitute(group))
+  if (length(groupName) > 0) {
+    group <- simpleFind(varName=groupName,data=data)
+    haveGroup <- TRUE
+  } else haveGroup <- FALSE
 
 # Define server logic for cloudSh app
 server <- shinyServer(function(input, output,session) {
 
+  output$zControl <- renderUI({
+    speed <- 1000-input$speed
+    sliderInput("zScreen","z",0,360,value=0,step=1,
+                animate=animationOptions(interval=speed,loop=TRUE))
+
+  })
+
+  output$xControl <- renderUI({
+    speed <- 1000-input$speed
+    sliderInput("xScreen","x",0,360,value=90,step=1,
+                animate=animationOptions(interval=speed,loop=TRUE))
+
+  })
+
+  output$yControl <- renderUI({
+    speed <- 1000-input$speed
+    sliderInput("yScreen","y",0,360,value=40,step=1,
+                animate=animationOptions(interval=speed,loop=TRUE))
+
+  })
+
   output$cloudplot <- renderPlot({
-    cloud(x,data=data)
+    if (!is.null(input$xScreen)) {
+      cloud(x,data=data,group=NULL,
+            screen=list(x=-input$xScreen,y=input$yScreen,z=input$zScreen),
+            auto.key=haveGroup)
+    }
   })
 
 
@@ -52,22 +73,17 @@ ui <- shinyUI(fluidPage(shinythemes::shinytheme("cerulean"),
   # Sidebar
   sidebarLayout(
     sidebarPanel(
-      helpText("One simulation means the machine will produce one table of",
-             "counts, using the Null probabilities.  How many simulations do",
-             "you want the machine to perform at once?  (Limit is 10000.)"),
-      numericInput("sims","Number of Simulations at Once",1,min=0,step=1),
-      br(),
-      actionButton("resample","Simulate Now"),
-      conditionalPanel(
-        condition="(input.resample > 0 && input.reset == 0) || output.total > output.totalPrev",
-        actionButton("reset","Start Over")
-      )
+        numericInput("speed","Animation Speed",0,900,step=100,value=450),
+        uiOutput("zControl"),
+        uiOutput("xControl"),
+        uiOutput("yControl")
     ), #end sidebarPanel
 
   # Here comes the main panel
   mainPanel(
-
-    plotOutput("cloudplot")
+    wellPanel(
+      plotOutput("cloudplot")
+    )
 
   ) #end mainPanel
 
